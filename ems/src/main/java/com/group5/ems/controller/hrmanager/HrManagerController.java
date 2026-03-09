e9aa1e4be33e005e5e99269acff15aeefe1e6af4
QUANG HIEU <quanghieuhg2005@gmail.com>
2026-03-09 13:20:09 +0700

    feat: add HrManagerController.java
    controller for demo

--- ems/src/main/java/com/group5/ems/controller/hrmanager/HrManagerController.java
+++ ems/src/main/java/com/group5/ems/controller/hrmanager/HrManagerController.java
@@ -1,9 +1,15 @@
 package com.group5.ems.controller.hrmanager;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hr-manager")
+@RequestMapping("/hrmanager")
 public class HrManagerController {
+    
+    @GetMapping({"", "/", "/index"})
+    public String index() {
+        return "hrmanager/index";
+    }
 }
