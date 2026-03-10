47a48b98cc882485b8f54360826dcdb561a25c55
QUANG HIEU <quanghieuhg2005@gmail.com>
2026-03-10 10:28:00 +0700

    chore: update mapping interfaces
    minor updates

--- ems/src/main/java/com/group5/ems/controller/hrmanager/HrManagerController.java
+++ ems/src/main/java/com/group5/ems/controller/hrmanager/HrManagerController.java
@@ -9,7 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HrManagerController {
     
     @GetMapping({"", "/", "/dashboard"})
-    public String index() {
+    public String dashboard() {
         return "hrmanager/dashboard";
+    }
+    
+    @GetMapping("/leave-approval")
+    public String leaveApproval() {
+        return "hrmanager/leave_approval";
+    }
+    
+    @GetMapping("/payroll-approval")
+    public String payrollApproval() {
+        return "hrmanager/payroll_approval";
+    }
+    
+    @GetMapping("/hr-analytics")
+    public String hrAnalytics() {
+        return "hrmanager/hr_analytics";
     }
 }
