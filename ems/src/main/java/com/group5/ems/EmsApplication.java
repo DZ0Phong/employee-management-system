6d56b3dfca7db7977007064622e666cb18c4169e
datdt69 <2imdanton139@gmail.com>
2026-03-12 22:26:42 +0700

    feat: add running info

--- ems/src/main/java/com/group5/ems/EmsApplication.java
+++ ems/src/main/java/com/group5/ems/EmsApplication.java
@@ -8,6 +8,12 @@ public class EmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmsApplication.class, args);
+        printStartupInfo();
+    }
+    public static void printStartupInfo(){
+        System.out.println("""
+                Server run at: http://localhost:8080
+                """);
     }
 
 }
